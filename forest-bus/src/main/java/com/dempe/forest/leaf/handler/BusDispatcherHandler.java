19a4cee5b00fd1e070a3a4ab21284acf040ef840
DempeZheng <752996510@qq.com>
2016-02-03 16:12:17 +0800

    1.实现业务框架 2.实现基于name服务器的高可用client

--- forest-bus/src/main/java/com/dempe/forest/leaf/handler/BusDispatcherHandler.java
+++ forest-bus/src/main/java/com/dempe/forest/leaf/handler/BusDispatcherHandler.java
@@ -5,13 +5,10 @@ import com.dempe.forest.common.proto.Request;
 import com.google.common.collect.Maps;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.util.concurrent.DefaultThreadFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,12 +21,7 @@ public class BusDispatcherHandler extends ChannelHandlerAdapter {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(BusDispatcherHandler.class);
 
-    // 业务逻辑线程池(业务逻辑最好跟netty io线程分开处理，线程切换虽会带来一定的性能损耗，但可以防止业务逻辑阻塞io线程)
-    private final static ExecutorService workerThreadService = Executors.newFixedThreadPool(
-            Runtime.getRuntime().availableProcessors(), new DefaultThreadFactory("workerThread"));
-
     private final static Map<String, HAClientService> nameClientMap = Maps.newConcurrentMap();
-
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
